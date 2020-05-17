b0dbecdf0edb8f0c9d33b0b917117d0f69d5d7cd
qqqlab <46283638+qqqlab@users.noreply.github.com>
2020-05-18 00:08:50 +0200

    Update qqq_adc.h

--- qqq_adc.h
+++ qqq_adc.h
@@ -47,36 +47,79 @@ int16_t temp_read_raw() {
   return v;
 }
 
-//returns temperature in 1/10 C
-uint16_t temp_read() {
-	#define OVERSAMPLE_BITS 4
+//get temperature in Celcius
+int16_t adc_temp_c() {
+	VREF.CTRLA = VREF_ADC0REFSEL_1V1_gc; //vref 1.1V
+	VREF.CTRLB = VREF_ADC0REFEN_bm;
+	//_delay_us(25); //settle vref
+	ADC0.CTRLC = ADC_REFSEL_INTREF_gc | ADC_PRESC_gc | ADC_SAMPCAP_bm; //select internal vref | F_CPU/x | select normal 5 pf (0 = normal size, 1 = reduced size)
+	ADC0.MUXPOS = ADC_MUXPOS_TEMPSENSE_gc; //select temp sensor
+	ADC0.CTRLD = ADC_INITDLY_DLY32_gc; // >= 32 cycles delay
+	ADC0.SAMPCTRL = 0x1f; //select >= 32us * fclk_adc ??
+	ADC0.CTRLB = ADC_SAMPNUM_ACC1_gc; //accumulate 1 sample
 
-	//uart_printf("temp_read:");
-	uint16_t adc_reading = temp_read_raw(); //dummy read - improves accuracy
-	adc_reading = 0;
-	for(uint8_t i=0;i<(1<<OVERSAMPLE_BITS);i++) {
-		uint16_t v = temp_read_raw();
-		//uart_printf("%d ",v);
-		adc_reading += v;
-	}
+	//start conversion
+	ADC0.CTRLA = ADC_ENABLE_bm;
+	ADC0.COMMAND = ADC_STCONV_bm;
+	while(ADC0_COMMAND & ADC_STCONV_bm);
+	uint16_t adc_reading = ADC0.RES; // ADC conversion result with 1.1 V internal reference
 
-	int16_t sigrow_offset = ((int16_t)SIGROW.TEMPSENSE1) * (1<<OVERSAMPLE_BITS);
-	uint8_t sigrow_gain = SIGROW.TEMPSENSE0;
-
+/*from datasheet:
+	int8_t sigrow_offset = SIGROW.TEMPSENSE1; // Read signed value from signature row
+	uint8_t sigrow_gain = SIGROW.TEMPSENSE0; // Read unsigned value from signature row
 	uint32_t temp = adc_reading - sigrow_offset;
-	//temp *= sigrow_gain; // Result might overflow 16 bit variable (10bit+8bit)
-	//temp += 0x80; // Add 1/2 to get correct rounding on division below
-	//temp >>= 8; // Divide result to get Kelvin
-	//uint16_t temperature_in_C = temp - 273;
+	temp *= sigrow_gain; // Result might overflow 16 bit variable (10bit+8bit)
+	temp += 0x80; // Add 1/2 to get correct rounding on division below
+	temp >>= 8; // Divide result to get Kelvin
+	uint16_t temperature_in_K = temp;
+*/
+	//adapted to Celcius
+	int8_t sigrow_offset = SIGROW.TEMPSENSE1; // Read signed value from signature row
+	uint8_t sigrow_gain = SIGROW.TEMPSENSE0; // Read unsigned value from signature row
+	uint32_t temp = adc_reading - sigrow_offset;
+	temp *= sigrow_gain; // Result might overflow 16 bit variable (10bit+8bit)
+	//temp is now K * 256
+	//need to add 0.5K for rounding
+	//need to subtract 0.15K to get to Celsius (0C = 273.15K)
+	//total: add 0.35K * 256 = 90
+	temp += 90; // Add 1/2 to get correct rounding on division below
+	temp >>= 8; // Divide result to get Celsius + 273
+	int16_t celsius = temp - 273;
 
-	temp *= sigrow_gain; // Result 14bit + 8bit = 22bit - temp in 1/(256 * OVERSAMPLE_NUM) kelvin
-	temp *= 10;   // temp in  1/(2560 * OVERSAMPLE_NUM) kelvin
-	//temp += (1<<(7+OVERSAMPLE_BITS)); // Add 1/2 to get correct rounding on division below
-	temp >>= (8+OVERSAMPLE_BITS);  // temp in 1/10 kelvin
-	temp -= 2731; // temp in 1/10 celsius
+	return celsius;
+}
 
-	//uart_printf(" raw=%d offset=%d gain=%d c=%d ",adc_reading, (int)sigrow_offset, (int)sigrow_gain, (int)temp);
-	return (uint16_t)temp;
+//get temperature in Celcius*10 (i.e. 245 is 24.5 Celcius)
+int16_t adc_temp_c10() {
+	VREF.CTRLA = VREF_ADC0REFSEL_1V1_gc; //vref 1.1V
+	VREF.CTRLB = VREF_ADC0REFEN_bm;
+	//_delay_us(25); //settle vref
+	ADC0.CTRLC = ADC_REFSEL_INTREF_gc | ADC_PRESC_gc | ADC_SAMPCAP_bm; //select internal vref | F_CPU/x | select normal 5 pf (0 = normal size, 1 = reduced size)
+	ADC0.MUXPOS = ADC_MUXPOS_TEMPSENSE_gc; //select temp sensor
+	ADC0.CTRLD = ADC_INITDLY_DLY32_gc; // >= 32 cycles delay
+	ADC0.SAMPCTRL = 0x1f; //select >= 32us * fclk_adc ??
+	ADC0.CTRLB = ADC_SAMPNUM_ACC64_gc; //accumulate 64 samples (16 bit result)
+
+	//start conversion
+	ADC0.CTRLA = ADC_ENABLE_bm;
+	ADC0.COMMAND = ADC_STCONV_bm;
+	while(ADC0_COMMAND & ADC_STCONV_bm);
+	uint16_t adc_reading = ADC0.RES; // ADC conversion result with 1.1 V internal reference
+
+	int8_t sigrow_offset = SIGROW.TEMPSENSE1; // Read signed value from signature row
+	int16_t sigrow_offset64 = sigrow_offset * 64;
+	uint8_t sigrow_gain = SIGROW.TEMPSENSE0; // Read unsigned value from signature row
+	uint32_t temp = adc_reading - sigrow_offset64;
+	temp *= sigrow_gain; // Result 16bit+8bit = 24bit - temp is now Kelvin * 64 * 256
+	temp *= 10; // Result now 28 bit = Kelvin * 64 * 256 * 10 = kelvin
+	//need to add 0.05K for rounding to next 0.1K
+	//need to subtract 0.05K to get to Celcius (0C = 273.15K)
+	//total: add 0
+	//divide by 64 * 256 (14 bits) to get Celcius*10 + 2731 value (don't shift bits, temp is signed)
+	temp >>= 14;
+	int16_t celsius10 = temp - 2731;
+
+	return celsius10;
 }
 
 uint16_t adc_get_result() {
